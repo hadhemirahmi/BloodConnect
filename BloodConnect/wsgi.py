d5e9622debd80fe924ed02befd088cead6dff3fa
hadhemirahmi <hadhemi93742594@gmail.com>
2026-05-07 23:22:16 +0100

    blood

--- BloodConnect/wsgi.py
+++ BloodConnect/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Chemin vers votre projet
+path = '/home/hadhemi/BloodConnect'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'BloodConnect.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BloodConnect.settings')
-
 application = get_wsgi_application()
+
