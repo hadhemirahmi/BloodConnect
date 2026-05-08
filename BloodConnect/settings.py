1ab2f9af1e60c63363d8aad2dc9953ab2d40c279
hadhemirahmi <hadhemi93742594@gmail.com>
2026-05-08 16:37:38 +0100

    feat: configure Gmail SMTP for password reset emails

--- BloodConnect/settings.py
+++ BloodConnect/settings.py
@@ -151,9 +151,15 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # =========================
-# EMAIL CONFIGURATION (Local testing)
+# EMAIL CONFIGURATION
 # =========================
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER', default='noreply@bloodconnect.com')
 
 
 # =========================
